89fc04e4b5faad7a5ee6a5a321347f6f90864bbe
amirwpf <hh1900dppp@gmail.com>
2023-05-19 10:44:33 +0430

    Models been added !

---
+++ CW_6_2/CW_6_2/Models/Empolyee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_6_2.Models
+{
+    public class Empolyee
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int NationalCode { get; set; }
+        public int Mobile { get; set; }
+        public int Gender { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
---
+++ CW_6_2/CW_6_2/Models/Salary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_6_2.Models
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int Tax { get; set; }
+        public int OtherBenefit { get; set; }
+        public int EmployeeId { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
