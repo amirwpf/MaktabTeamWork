711d6ed1f2b9c339191489748be93e1925ac658a
mmd <mhmdttlbzd@gmail.com>
2023-05-19 12:10:32 +0430

    'interface'

---
+++ CW_6_2/CW_6_2/Repository/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_6_2.Repository
+{
+    internal interface IEmployeeService
+    {
+        List<EmployeeNameViewModel> GetEmployeeName();
+        List<EmployeeNameViewModel> GetMenEmpName();
+    }
+}
